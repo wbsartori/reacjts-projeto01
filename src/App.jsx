e2c21251fde80f798f4980cf9b71dd9e50b4e8aa
wbsartori <wbsartori@gmail.com>
2022-10-21 22:38:01 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import {Header} from "./components/Header";
 export function App() {
   return (
       <div>
-        <h1>Hello world!</h1>
         <Header />
       </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css';
 
+import igniteLogo from '../assets/images/ignite-logo.svg';
+
 export function Header(){
     return (
         <header className={styles.header}>
-            <strong>Ignite feed</strong>
+            <img src="src/assets/images/ignite-logo.svg"/>
         </header>
     );
 }
