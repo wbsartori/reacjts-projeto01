5e7abc0414d7ae0d5acf8d6edf96720e360a6f78
wbsartori <wbsartori@gmail.com>
2022-10-22 14:25:29 -0300

    count dos likes.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,13 +2,18 @@ import styles from './Comment.module.css';
 
 import {ThumbsUp, Trash} from "phosphor-react";
 import {Avatar} from "./Avatar";
+import {useState} from "react";
 
 export function Comment({ content, onDeleteComment }){
+    const [likeCount, setLikeCount] = useState(0);
 
     function handleDeleteComment(){
         onDeleteComment(content);
     }
 
+    function handleLikeComment(){
+        setLikeCount(likeCount + 1);
+    }
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/25470180?v=4"/>
@@ -31,9 +36,9 @@ export function Comment({ content, onDeleteComment }){
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>28</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
