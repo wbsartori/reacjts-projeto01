f3d08960f84d4a597bdb79e48b5cc00805474555
wbsartori <wbsartori@gmail.com>
2022-10-22 00:44:13 -0300

    post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -25,6 +25,16 @@ export function Post(){
                     <a href="#"> #rocketseat</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea placeholder="Deixe seu comentário" />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
