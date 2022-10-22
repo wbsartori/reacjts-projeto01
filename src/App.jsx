0de394484d19416bbf08b4dba15eb51c160c04cb
wbsartori <wbsartori@gmail.com>
2022-10-22 00:21:49 -0300

    post

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Header} from "./components/Header";
 
 import styles from './App.module.css';
 import {Sidebar} from "./components/Sidebar";
+import {Post} from "./components/Post";
 
 export function App() {
   return (
@@ -10,7 +11,7 @@ export function App() {
           <div className={styles.wrapper}>
             <Sidebar />
               <main>
-                Postagens
+                <Post />
               </main>
           </div>
       </div>
