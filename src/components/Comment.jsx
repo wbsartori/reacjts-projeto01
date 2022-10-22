c192d1eb897a1f7b267f4a7009d243b56666d491
wbsartori <wbsartori@gmail.com>
2022-10-22 12:36:03 -0300

    ajuste component Comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import styles from './Comment.module.css';
 import {ThumbsUp, Trash} from "phosphor-react";
 import {Avatar} from "./Avatar";
 
-export function Comment(){
+export function Comment({ content }){
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/25470180?v=4"/>
@@ -21,7 +21,7 @@ export function Comment(){
                         </button>
                     </header>
 
-                    <p>Muito bom Devon, parabéns ! </p>
+                    <p>{content}</p>
                 </div>
                 <footer>
                     <button>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,17 @@
+import {useState} from "react";
+
 import {format, formatDistanceToNow} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
+
 import styles from './Post.module.css';
 
 import {Comment} from "./Comment";
 import {Avatar} from "./Avatar";
 
 export function Post({author, publishedAt, content}) {
+    const [comments, setComments] = useState([]);
+    const[newCommentText, setNewCommentText] = useState('');
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR
     });
@@ -13,7 +19,18 @@ export function Post({author, publishedAt, content}) {
     const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
         locale: ptBR,
         addSuffix: true
-    })
+    });
+
+    function handleCreateNewComment(){
+        event.preventDefault();
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange(){
+        setNewCommentText(event.target.value);
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -40,10 +57,15 @@ export function Post({author, publishedAt, content}) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
-                <textarea placeholder="Deixe seu comentário"/>
+                <textarea
+                    name="comment"
+                    placeholder="Deixe seu comentário"
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
+                />
 
                 <footer>
                     <button type="submit">Publicar</button>
@@ -51,9 +73,9 @@ export function Post({author, publishedAt, content}) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
+                {comments.map(comment => {
+                    return <Comment content={comment}/>
+                })}
             </div>
         </article>
     )
