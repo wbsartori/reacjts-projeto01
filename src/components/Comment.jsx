453834ba662447ac263ee55271ab8609709173d2
wbsartori <wbsartori@gmail.com>
2022-10-22 09:51:12 -0300

    Comment e alteração dos nomes das variaveis globais do css.

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import styles from './Comment.module.css';
+
+import {ThumbsUp, Trash} from "phosphor-react";
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/25470180?v=4" alt=""/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Wesley Sartóri</strong>
+                            <time title="11 de maio às 08:13h" dateTime="2022-05-11 08:13:33">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns ! </p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>28</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import styles from './Post.module.css';
+import {Comment} from "./Comment";
 
 export function Post(){
     return (
@@ -35,6 +36,12 @@ export function Post(){
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
         </article>
     )
 }
